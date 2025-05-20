89b3b9bef1240d785cd9b50fd57efa84db91b1dd
mf751 <mf751f@gmail.com>
2025-05-20 14:33:46 +0300

    feat: chat add icon

--- cmd/frontend/src/pages/home/index.jsx
+++ cmd/frontend/src/pages/home/index.jsx
@@ -1,9 +1,12 @@
 import { useSelector } from "react-redux";
 import "./styles.css";
 import Chat from "./components/chat.jsx";
+import { useState } from "react";
+import { IoIosAddCircle } from "react-icons/io";
 
 export default function Home() {
   const chats = useSelector((state) => state.chats.chats);
+  const [addShown, setAddShown] = useState(false);
 
   if (chats === null || chats.length === 0) {
     return <h1>You Don't Have Any Chats</h1>;
@@ -20,6 +23,9 @@ export default function Home() {
             lastMessage={obj.last_message}
           />
         ))}
+        <div className="add-chat">
+          <IoIosAddCircle className="icon" />
+        </div>
       </div>
     </div>
   );
